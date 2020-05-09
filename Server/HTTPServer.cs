44bf26fc2fb5bba84037c02082162fa3e1aea736
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-05-09 00:02:09 -0300

    Servidor Multithread

--- Server/HTTPServer.cs
+++ Server/HTTPServer.cs
@@ -12,7 +12,8 @@ namespace Server {
     class HTTPServer {
         private static string _host0 = "http://127.0.0.1:42069/";
         private static string _host1 = "http://localhost:42069/";
-        private int i = 0;
+        private int _tid;
+        
         public HTTPServer() {
             var httpListener = new HttpListener();
             httpListener.Prefixes.Add(_host0);
@@ -21,11 +22,11 @@ namespace Server {
 
             while(httpListener.IsListening) {
                 var thread = new Thread(ProccessRequest);
-                thread.Name = (++i).ToString();
-                thread.Start(httpListener.GetContext());
-                
+                thread.Name = (++_tid).ToString();
+                thread.Start(httpListener.GetContext()); 
                 Console.WriteLine($"Iniciou a Thread:  {thread.Name}");
             }
+            
             httpListener.Close();
         }
 
@@ -54,18 +55,20 @@ namespace Server {
             var request = context.Request;
             var reqPath = request.Url.GetComponents(UriComponents.Path, UriFormat.SafeUnescaped);
             
-            // Pega as medidas do usuario no DB
+            // tenta converter reqPath, caso consiga retorna true e altera o valor de uid
             int uid;
             dynamic resp = (Int32.TryParse(reqPath, out uid)) ? DataBase.GetMedidas(uid) : null;
             
-            // Serializa em JSON 
+            // Converte as mensagens para Json e as coloca numa lista
             if(resp != null) {
                 List<string> list = new List<string>();
                 foreach(Message message in resp) list.Add(message.ToJson());
                 resp = JsonConvert.SerializeObject(list);
+                this.SendResponse(context.Response, resp);
             }
-            
-            this.SendResponse(context.Response, resp);
+            else
+                this.SendResponse(context.Response, resp, 400);
+
         }
 
         private void SendResponse(HttpListenerResponse response, string? resp, int status=200) {
@@ -79,12 +82,14 @@ namespace Server {
 
         private void ProccessRequest(object? o) {
             var context = (HttpListenerContext) o;
-            Console.WriteLine("Recebeu um request: " + context.Request.HttpMethod);
+            Console.WriteLine($"Thread: {Thread.CurrentThread.Name} " +
+                              $"Recebeu um request: {context.Request.HttpMethod}");
+            
             if(context.Request.HttpMethod == HttpMethod.Get.Method) this.HandleGet(context);
             else if(context.Request.HttpMethod == HttpMethod.Post.Method) this.HandlePost(context);
             
             Console.WriteLine($"Terminou a thread {Thread.CurrentThread.Name}");
-            i--;
+            _tid--;
         }
         
         public static void Main(string[] args) {
