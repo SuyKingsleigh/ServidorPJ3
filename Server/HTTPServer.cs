70d10c4f2cd099caff28b276e84a33b4114093c7
SuyKingsleigh <suyan.m@aluno.ifsc.edu.br>
2020-05-08 23:39:51 -0300

    Servidor Assincrono.

--- Server/HTTPServer.cs
+++ Server/HTTPServer.cs
@@ -6,19 +6,12 @@ using System.Net;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server {
     class HTTPServer {
         private static string _host = "http://127.0.0.1:42069/";
-
-        public HTTPServer() {
-            var httpListener = new HttpListener();
-            httpListener.Prefixes.Add(_host);
-            httpListener.Start();
-
-            while(httpListener.IsListening) ProccessRequest(httpListener.GetContext());
-            httpListener.Close();
-        }
 
         /**
          * Adiciona uma mensagem ao banco de dados 
@@ -37,7 +30,7 @@ namespace Server {
 
         /**
          * Solicita as mensagens de um determinado usuario (uid)
-         * Envia ao cliente a lista de mensanges recebidas
+         * Envia ao cliente a lista de mensagens(medidas) recebidas
          */
         private void HandleGet(HttpListenerContext context) {
             // no get vem tudo na URL
@@ -51,32 +44,47 @@ namespace Server {
             
             // Serializa em JSON 
             if(resp != null) {
-                List<string> list = new List<string>();
+                var list = new List<string>();
                 foreach(Message message in resp) list.Add(message.ToJson());
                 resp = JsonConvert.SerializeObject(list);
+                this.SendResponse(context.Response, resp);
             }
-            
-            this.SendResponse(context.Response, resp);
+            else
+                this.SendResponse(context.Response, resp, 400);
         }
 
         private void SendResponse(HttpListenerResponse response, string? resp, int status=200) {
-            var buffer = System.Text.Encoding.UTF8.GetBytes(resp ?? "UID deve ser int");
+            var buffer = System.Text.Encoding.UTF8.GetBytes(resp ?? $"Erro: {status}");
             response.StatusCode = status;
-            response.ContentLength64 = buffer.Length;   // Define o tamanho da mensagem 
+            response.ContentLength64 = buffer.Length;  // Define o tamanho da mensagem 
             System.IO.Stream output = response.OutputStream; // Abre um stream de saia 
             output.Write(buffer,0,buffer.Length);  
             output.Close();
         }
 
-        private void ProccessRequest(HttpListenerContext context) {
-            Console.WriteLine("Recebeu um request: " + context.Request.HttpMethod);
+        private async Task ProccessRequest(Task<HttpListenerContext> con ) {
+            var context = await con; // espera o resultado do request e entao o processa 
+            Console.WriteLine($"Recebeu um request: {context.Request.HttpMethod}");
             if(context.Request.HttpMethod == HttpMethod.Get.Method) this.HandleGet(context);
             else if(context.Request.HttpMethod == HttpMethod.Post.Method) this.HandlePost(context);
         }
-        
-        public static void Main(string[] args) {
+
+        public async Task Run() {
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add(_host);
+            httpListener.Start();
+            
+            // para cada request recebido, ira pegar os dados 
+            // mas nao ficará bloqueado no mesmo 
+            // assim consegue processar varios de uma vez. 
+            while(httpListener.IsListening) 
+                await this.ProccessRequest(httpListener.GetContextAsync());
+            httpListener.Close();
+        }
+
+        public static async Task Main(string[] args) {
             try {
-                HTTPServer server = new HTTPServer();
+                await new HTTPServer().Run();
             }
             catch(Exception e) {
                 Console.WriteLine(e);
--- Server/client.js
+++ Server/client.js
@@ -4,11 +4,11 @@ const _host = "127.0.0.1";
 const _port = 42069;
 // const _data = '{"4/2020", "temp" : 32, "uid" : 1}'; // com erro
 const _data = '{"date" : "21:39:45 5/4/2020", "temp" : 32, "uid" : 1}';
-
+var _uid = 1; 
 const getOptions = {
     host: _host,
     port: _port,
-    path: '/1'
+    path: '/' + _uid
 };
 
 const postOptions = {
@@ -20,18 +20,21 @@ const postOptions = {
         'Content-Length': _data.length
     }
 };
+for (let i = 0; i < 500; i++) {
+    _uid = (i%2)+1;
+    var req_get = http.get(getOptions, res => {
+        res.setEncoding('utf8');
+        // res.on('data', chunk => console.log("Chunk: ", chunk));
+        res.on('data', chunk => console.log("i: " + i));
+    });
+    req_get.end();
+}
 
-var req_get = http.get(getOptions, res => {
-    res.setEncoding('utf8');
-    res.on('data', chunk => console.log("Chunk: ", chunk));
-});
-req_get.end();
+// var req_post = http.request(postOptions, res => {
+//     console.log('StatusCode: ', res.statusCode);
+//     res.setEncoding('utf8');
+//     res.on('data', chunk => console.log(chunk));
+// });
 
-var req_post = http.request(postOptions, res => {
-    console.log('StatusCode: ', res.statusCode);
-    res.setEncoding('utf8');
-    res.on('data', chunk => console.log(chunk));
-});
-
-req_post.write(_data);
-req_post.end();
+// req_post.write(_data);
+// req_post.end();
